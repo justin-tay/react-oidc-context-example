31e8487f2ea644ec2037a936cae29a8d31db3b49
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2026-01-23 23:45:15 +0800

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,45 @@ import viteLogo from "./assets/vite.svg";
 import { useAuth } from "react-oidc-context";
 import "./App.css";
 import { useSessionStorage } from "./useSessionStorage";
+import { Link, Route, Routes } from "react-router-dom";
+
+function Home() {
+  const [count, setCount] = useSessionStorage("count", 0);
+
+  return (
+    <>
+      <h2>Home Page</h2>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+function About() {
+  return <h2>About Page</h2>;
+}
+function Contact() {
+  return <h2>Contact Page</h2>;
+}
 
 function App() {
   const auth = useAuth();
-
-  const [count, setCount] = useSessionStorage('count', 0)
 
   if (auth.isLoading) {
     return <div>Loading authentication...</div>;
@@ -20,29 +54,29 @@ function App() {
   if (auth.isAuthenticated) {
     return (
       <>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <div>
-          <p>Hello, {auth.user?.profile.name}!</p>
-          <button onClick={() => void auth.signoutRedirect()}>Log out</button>
-        </div>
-        <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <nav className="navbar">
+          <div className="nav-left">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>{" "}
+            <Link to="/about" className="nav-link">
+              About
+            </Link>{" "}
+            <Link to="/contact" className="nav-link">
+              Contact
+            </Link>
+          </div>
+          <div className="nav-right">
+            <div className="nav-link">Hello, {auth.user?.profile.name}!<button onClick={() => void auth.signoutRedirect()}>Log out</button></div>
+            
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </>
     );
   }
--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider, type AuthProviderProps } from "react-oidc-context";
+import { BrowserRouter } from "react-router-dom";
 
 const issuer = "http://localhost:8080/realms/test";
 
 const oidcConfig: AuthProviderProps = {
   authority: issuer,
   client_id: "react-oidc-context-example",
-  redirect_uri: window.location.origin,
+  redirect_uri: window.location.origin + window.location.pathname + window.location.hash,
   onSigninCallback: () => {
     // Clear the auth code parameters from the URL after login
-    window.history.replaceState({}, document.title, window.location.pathname);
+    window.history.replaceState({}, document.title, window.location.pathname + window.location.hash);
   },
   post_logout_redirect_uri: window.location.origin, // Ask keycloak to redirect back to app after logout instead of displaying the keycloak logout page
   metadata: { // Define meta data manually instead of requiring a call to the keycloak discovery endpoint
@@ -28,7 +29,9 @@ const oidcConfig: AuthProviderProps = {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider {...oidcConfig}>
-    <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </StrictMode>,
 )
